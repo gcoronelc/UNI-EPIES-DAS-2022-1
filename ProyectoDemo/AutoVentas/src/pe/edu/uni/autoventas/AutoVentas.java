8f55101e3eaefee5a1d42f1f268049532bbe06c5
Gustavo Coronel <gcoronelc@gmail.com>
2022-06-09 16:11:36 -0500

    Semana 09

---
+++ ProyectoDemo/AutoVentas/src/pe/edu/uni/autoventas/AutoVentas.java
@@ -0,0 +1,17 @@
+package pe.edu.uni.autoventas;
+
+/**
+ * @author Eric Gustavo Coronel Castillo
+ * @blog www.desarrollasoftware.com
+ * @email gcoronelc@gmail.com
+ * @youtube www.youtube.com/DesarrollaSoftware
+ * @facebook www.facebook.com/groups/desarrollasoftware
+ * @cursos gcoronelc.github.io
+ */
+public class AutoVentas {
+
+    public static void main(String[] args) {
+
+    }
+
+}
